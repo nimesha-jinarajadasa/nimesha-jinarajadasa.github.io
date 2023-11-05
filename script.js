68cc2d9c8a8ba9ffad2f9bb0045a6ab455984ce7
darshana494 <darshanawishwajith494@gmail.com>
2023-11-06 02:07:55 +0530

    Added a resume section(incomplete)

---
+++ script.js
@@ -0,0 +1,32 @@
+//Navbar active state stylings
+$(document).ready(function(){
+
+    $('.nav').click(function(){
+
+        $('a').removeClass('active');
+        $('.contact-btn').removeClass('active');
+        
+        $(this).addClass('active');
+
+
+    });
+});
+
+function showDesc(id){
+
+    
+
+    var descId = "desc"+id[5];
+
+    for(var i = 1; i <= 7; i++){
+
+        if(i == id[5]){
+            document.getElementById(descId).classList = descId+" "+"d-inline";
+            document.getElementById(id).classList = id+" "+"fw-bold active-timeline";
+            continue
+        }
+        document.getElementById("desc"+i.toString()).classList = "desc"+i.toString()+" "+"d-none";
+        document.getElementById(id).classList = id+" "+"fw-bold";
+        
+    }
+}
