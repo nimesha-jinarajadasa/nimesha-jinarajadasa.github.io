1e4dfa3a3919b78c5ba23545ca07f572e7e7cd95
darshana494 <darshanawishwajith494@gmail.com>
2023-11-22 13:36:14 +0530

    Did some optimizations in mobile view

--- script.js
+++ script.js
@@ -181,7 +181,7 @@ discordRequest.onreadystatechange = function(){
   }
 }
 
-discordRequest.open("GET", "https://discord.com/api/v9/invites/mTGpeMQf?with_counts=true&with_expiration=false", true);
+discordRequest.open("GET", "https://discord.com/api/v9/invites/mTGpeMQf?with_counts=true&with_expiration=true", true);
 
 discordRequest.send();
 
